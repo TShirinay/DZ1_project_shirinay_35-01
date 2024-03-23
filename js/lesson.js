1069c3ca2a4194471cc4df98236dd44e314ec598
TShirinay <sirinajtemirova@gmail.com>
2024-03-23 12:08:27 +0600

    dz5

--- js/lesson.js
+++ js/lesson.js
@@ -61,3 +61,52 @@ const showSlider = (i = 0) => {
     },3000)
 }
 showSlider()
+
+
+// CONVERTER
+
+const usdInput = document.querySelector('#usd')
+const somInput = document.querySelector('#som')
+const eurInput = document.querySelector('#eur')
+const converter = (element, targetElement, targetElement2, current) => {
+    element.oninput = () => {
+        const request = new XMLHttpRequest()
+        request.open('GET', '../data/converter.json')
+        request.setRequestHeader('content-type', 'application/json')
+        request.send()
+
+        request.onload = () => {
+            const data = JSON.parse(request.response)
+
+            switch (current) {
+                case 'som':
+                    targetElement.value = (element.value / data.usd).toFixed(2)
+                    targetElement2.value = (element.value / data.eur).toFixed(2)
+                    break
+                case 'usd':
+                    targetElement.value = (element.value / data.usd).toFixed(2)
+                    targetElement2.value = (element.value *  data.eur / data.usd).toFixed(2)
+                    break
+                case 'eur':
+                    targetElement.value = (element.value * data.eur).toFixed(2)
+                    targetElement2.value = (element.value * (data.usd / data.eur)).toFixed(2)
+                default:
+                    break
+            }
+
+            element.value === '' && (targetElement.value = '')
+            if(element.value === '' || targetElement.value ==='') {
+                targetElement.value = ''
+                targetElement2.value = ''
+            }
+
+        }
+    }
+}
+
+converter(somInput,usdInput,eurInput, 'som')
+converter(usdInput,somInput,eurInput, 'usd')
+converter(eurInput,somInput,usdInput,'eur')
+
+// DRY - don`t repeat yourself
+// KISS - keep it simple, stupid
